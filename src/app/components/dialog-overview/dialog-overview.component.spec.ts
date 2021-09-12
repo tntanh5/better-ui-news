0ba0090367a330002e242c3e906d2dfbda30a253
tntanh5 <tntanh5@gmail.com>
2021-09-12 15:49:13 +0000

    update test and get chance to test deployment

--- src/app/components/dialog-overview/dialog-overview.component.spec.ts
+++ src/app/components/dialog-overview/dialog-overview.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { DialogOverviewComponent } from './dialog-overview.component';
 
@@ -8,9 +9,17 @@ describe('DialogOverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DialogOverviewComponent ]
+      imports: [MatDialogModule],
+      declarations: [DialogOverviewComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        {
+          provide: MatDialogRef, useValue: {}
+        },
+        MatDialog
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
